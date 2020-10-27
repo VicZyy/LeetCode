4737a6d53ad39cd2cca0c6e97ebaa1ae707f246c
VicZyy <319478851@qq.com>
2020-10-27 23:14:13 +0800

    [1] 两数之和

--- Code/1.两数之和.cs
+++ Code/1.两数之和.cs
@@ -6,8 +6,26 @@
 
 // @lc code=start
 public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-
+    public int[] TwoSum(int[] nums, int target)
+    {
+        if(nums.Length>=2)   
+        {
+            for(int i=0;i<nums.Length-1;i++)
+            {
+                for(int j=i+1;j<nums.Length;j++)
+                {
+                    if(nums[i]+nums[j]==target)
+                    {
+                        return new int[2]{i,j};
+                    }
+                }
+            }
+         }
+        else
+        {
+            return null;
+        }
+        return null;
     }
 }
 // @lc code=end
